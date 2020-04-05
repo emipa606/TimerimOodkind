fddb1bf1910779693a990b67beac8f839141f3b8
Mlie <emipa606@users.noreply.github.com>
2020-04-05 21:47:24 +0200

    First publish

---
+++ Source/TimerimFactions/TimerimFactions/OodSong.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+
+namespace TimerimFactions
+{
+    public class IncidentWorker_OodSong : IncidentWorker_PsychicEmanation
+    {
+        protected override void DoConditionAndLetter(Map map, int duration, Gender gender)
+        {
+            GameCondition_PsychicEmanation gameCondition_PsychicEmanation = (GameCondition_PsychicEmanation)GameConditionMaker.MakeCondition(GameConditionDefOf.PsychicSoothe, duration, 0);
+            gameCondition_PsychicEmanation.gender = gender;
+            map.gameConditionManager.RegisterCondition(gameCondition_PsychicEmanation);
+            string text = "LetterIncidentOodSong".Translate(new object[]
+            {
+                gender.ToString().Translate().ToLower()
+            });
+
+            Find.LetterStack.ReceiveLetter("LetterLabelOodSong".Translate(), text, LetterDefOf.Good, null);
+        }
+
+    }
+}
+
---
+++ Source/TimerimFactions/TimerimFactions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TimerimFactions")]
+[assembly: AssemblyDescription("Timerim - Factions")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Demeggy")]
+[assembly: AssemblyProduct("TimerimFactions")]
+[assembly: AssemblyCopyright("Copyright ©Demeggy  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("812e1cbc-2588-4b26-8419-2d81dfe42630")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
